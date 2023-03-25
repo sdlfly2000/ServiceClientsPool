799ef820d231ddd15f2da80c5e92bc79fda5af42
Jay Shi <25982097@qq.com>
2023-03-25 16:42:17 +0800

    Update ServiceClientPool.cs
    Remove sensitive information

--- ServiceClientPool.cs
+++ ServiceClientPool.cs
@@ -14,7 +14,7 @@ namespace CoreCore
 
         public ServiceClientPool()
         {
-            var ConnectionString = @"authtype=OAuth;username=admin@CRM717490.onmicrosoft.com;password=4rSHm4156H;url=https://orgd80f911a.crm5.dynamics.com;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d; RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;loginPrompt=Auto";
+            var ConnectionString = @"authtype=OAuth;username=;password=;url=;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d; RedirectUri=app://58145B91-0C36-4500-8554-080854F2AC97;loginPrompt=Auto";
             _lazyServiceClient = new Lazy<ServiceClient>(() => GetServiceClient(ConnectionString), true);
         }
 
